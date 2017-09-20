c9ce7248689fdc35901eae748fea76455f1a4101
Salvador Martinez-Bartolome <salvador@scripps.edu>
2017-09-20 15:37:27 -0700

    use reentrantlock for rawfilenames

--- src/main/java/edu/scripps/yates/census/read/model/StaticQuantMaps.java
+++ src/main/java/edu/scripps/yates/census/read/model/StaticQuantMaps.java
@@ -1,6 +1,9 @@
 package edu.scripps.yates.census.read.model;
 
 import java.util.Set;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.ReadLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
 
 import org.apache.log4j.Logger;
 
@@ -13,13 +16,13 @@ import gnu.trove.set.hash.THashSet;
 public class StaticQuantMaps {
 	private final static Logger log = Logger.getLogger(StaticQuantMaps.class);
 
-	public static Set<String> rawFileNames = new THashSet<String>();
+	private static Set<String> rawFileNames = new THashSet<String>();
+	private final static ReentrantReadWriteLock rawFileNamesLock = new ReentrantReadWriteLock();
 	/**
 	 * The map that stores the {@link QuantifiedPSMInterface} by the spectrum
 	 * key
 	 */
 	public static StaticItemStorage<QuantifiedPSMInterface> psmMap = new StaticItemStorage<QuantifiedPSMInterface>();
-
 	// public static Map<String, QuantifiedPSMInterface> psmMap = new
 	// HashMap<String, QuantifiedPSMInterface>();
 
@@ -35,20 +38,50 @@ public class StaticQuantMaps {
 	public static StaticItemStorage<QuantifiedPeptideInterface> peptideMap = new StaticItemStorage<QuantifiedPeptideInterface>();
 
 	public static void clearInfo() {
+
 		log.info("Clearing static quantitative maps");
 		if (!proteinMap.isEmpty()) {
 			log.info("Clearing quant protein map");
 		}
 		proteinMap.clear();
+
 		if (!psmMap.isEmpty()) {
 			log.info("Clearing quant PSM map");
 		}
 		psmMap.clear();
+
 		if (!peptideMap.isEmpty()) {
 			log.info("Clearing quant peptide map");
 		}
 		peptideMap.clear();
-		rawFileNames.clear();
+
+		WriteLock writeLock = rawFileNamesLock.writeLock();
+		try {
+			writeLock.lock();
+			rawFileNames.clear();
+		} finally {
+			writeLock.unlock();
+		}
+	}
+
+	public static boolean rawFileNamesContains(String rawFileName) {
+		ReadLock readLock = rawFileNamesLock.readLock();
+		try {
+			readLock.lock();
+			return rawFileNames.contains(rawFileName);
+		} finally {
+			readLock.unlock();
+		}
+	}
+
+	public static void addRawFileName(String rawFileName) {
+		WriteLock writeLock = rawFileNamesLock.writeLock();
+		try {
+			writeLock.lock();
+			rawFileNames.add(rawFileName);
+		} finally {
+			writeLock.unlock();
+		}
 	}
 
 }
