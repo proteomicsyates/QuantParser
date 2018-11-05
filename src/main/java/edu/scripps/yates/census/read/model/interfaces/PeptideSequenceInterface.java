312b5d9762a4d7c5262e669a57841abd40517d55
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2018-11-05 15:23:02 -0800

    added new method to implement getPTMsInProteins

--- src/main/java/edu/scripps/yates/census/read/model/interfaces/PeptideSequenceInterface.java
+++ src/main/java/edu/scripps/yates/census/read/model/interfaces/PeptideSequenceInterface.java
@@ -1,9 +1,14 @@
 package edu.scripps.yates.census.read.model.interfaces;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import edu.scripps.yates.annotations.uniprot.UniprotProteinLocalRetriever;
 import edu.scripps.yates.utilities.sequence.PTMInPeptide;
+import edu.scripps.yates.utilities.sequence.PTMInProtein;
+import edu.scripps.yates.utilities.sequence.PositionInPeptide;
+import edu.scripps.yates.utilities.sequence.PositionInProtein;
 
 public interface PeptideSequenceInterface extends HasKey {
 	public String getSequence();
@@ -19,4 +24,19 @@ public interface PeptideSequenceInterface extends HasKey {
 	public boolean containsPTMs();
 
 	public List<PTMInPeptide> getPtms();
+
+	/**
+	 * Get a list of {@link PositionInProtein} for each ptm site in the peptide
+	 * sequence (represented as a {@link PositionInPeptide}).<br>
+	 * Examples:<br>
+	 * "PEPTIDE#4 {PROTEIN1#234#238, PROTEIN2#123#127}
+	 * 
+	 * @param uplr
+	 *            used in order to get the protein sequence
+	 * @param proteinSequences
+	 *            map of protein sequences
+	 * @param dBIndex
+	 * @return
+	 */
+	public List<PTMInProtein> getPTMInProtein(UniprotProteinLocalRetriever uplr, Map<String, String> proteinSequences);
 }
