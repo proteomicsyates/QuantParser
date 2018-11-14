8a1004e01898b9a68a8869f5cc61d61af9ceed64
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2018-11-13 16:11:23 -0800

    quant labels different from TMT6Plex and TMT10Plex

--- src/main/java/edu/scripps/yates/census/read/util/QuantificationLabel.java
+++ src/main/java/edu/scripps/yates/census/read/util/QuantificationLabel.java
@@ -1,14 +1,37 @@
 package edu.scripps.yates.census.read.util;
 
+import java.util.Set;
+
 import org.apache.log4j.Logger;
+
+import gnu.trove.set.hash.THashSet;
 
 public enum QuantificationLabel {
 
-	LIGHT(true, false, false), HEAVY(false, false, true), MEDIUM(false, true, false), //
-	TMT_6PLEX_126(true, false, false), TMT_6PLEX_127(false, false, false), TMT_6PLEX_128(false, false,
-			false), TMT_6PLEX_129(false, false,
-					false), TMT_6PLEX_130(false, false, false), TMT_6PLEX_131(false, false, true), //
-	N14(true, false, false), N15(false, false, true);
+	LIGHT(true, false, false), //
+	HEAVY(false, false, true), //
+	MEDIUM(false, true, false), //
+	//
+	TMT_6PLEX_126(true, false, false), //
+	TMT_6PLEX_127(false, false, false), //
+	TMT_6PLEX_128(false, false, false), //
+	TMT_6PLEX_129(false, false, false), //
+	TMT_6PLEX_130(false, false, false), //
+	TMT_6PLEX_131(false, false, true), //
+	//
+	TMT_10PLEX_126_127726(true, false, false), //
+	TMT_10PLEX_127_124761(false, false, false), //
+	TMT_10PLEX_127_131081(false, false, false), //
+	TMT_10PLEX_128_128116(false, false, false), //
+	TMT_10PLEX_128_134436(false, false, false), //
+	TMT_10PLEX_129_131471(false, false, false), //
+	TMT_10PLEX_129_13779(false, false, false), //
+	TMT_10PLEX_130_134825(false, false, false), //
+	TMT_10PLEX_130_141145(false, false, false), //
+	TMT_10PLEX_131_13818(false, false, true), //
+	//
+	N14(true, false, false), //
+	N15(false, false, true);
 	private final static Logger log = Logger.getLogger(QuantificationLabel.class);
 	private final boolean isLight;
 	private final boolean isMedium;
@@ -108,5 +131,38 @@ public enum QuantificationLabel {
 			}
 		}
 		return false;
+	}
+
+	public static boolean isTMT6PLEX(QuantificationLabel label) {
+		return getTMT6PlexLabels().contains(label);
+	}
+
+	public static boolean isTMT10PLEX(QuantificationLabel label) {
+		return getTMT10PlexLabels().contains(label);
+	}
+
+	public static Set<QuantificationLabel> getTMT6PlexLabels() {
+		final QuantificationLabel[] array = { QuantificationLabel.TMT_6PLEX_126, QuantificationLabel.TMT_6PLEX_127,
+				QuantificationLabel.TMT_6PLEX_128, QuantificationLabel.TMT_6PLEX_129, QuantificationLabel.TMT_6PLEX_130,
+				QuantificationLabel.TMT_6PLEX_131 };
+		final Set<QuantificationLabel> ret = new THashSet<QuantificationLabel>();
+		for (final QuantificationLabel quantificationLabel : array) {
+			ret.add(quantificationLabel);
+		}
+		return ret;
+	}
+
+	public static Set<QuantificationLabel> getTMT10PlexLabels() {
+		final QuantificationLabel[] array = { QuantificationLabel.TMT_10PLEX_126_127726,
+				QuantificationLabel.TMT_10PLEX_127_124761, QuantificationLabel.TMT_10PLEX_127_131081,
+				QuantificationLabel.TMT_10PLEX_128_128116, QuantificationLabel.TMT_10PLEX_128_134436,
+				QuantificationLabel.TMT_10PLEX_129_131471, QuantificationLabel.TMT_10PLEX_129_13779,
+				QuantificationLabel.TMT_10PLEX_130_134825, QuantificationLabel.TMT_10PLEX_130_141145,
+				QuantificationLabel.TMT_10PLEX_131_13818 };
+		final Set<QuantificationLabel> ret = new THashSet<QuantificationLabel>();
+		for (final QuantificationLabel quantificationLabel : array) {
+			ret.add(quantificationLabel);
+		}
+		return ret;
 	}
 }
