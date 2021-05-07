adab2198224da8dc85f2f1937bc084cdb57471c2
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2021-05-06 17:24:34 -0700

    TMT with 9 labels

--- src/main/java/edu/scripps/yates/census/read/util/QuantificationLabel.java
+++ src/main/java/edu/scripps/yates/census/read/util/QuantificationLabel.java
@@ -24,6 +24,16 @@ public enum QuantificationLabel {
 	TMT_6PLEX_130(false, false, false), //
 	TMT_6PLEX_131(false, false, true), //
 	//
+	TMT_9PLEX_126(true, false, false), //
+	TMT_9PLEX_127N(false, false, false), //
+	TMT_9PLEX_127C(false, false, false), //
+	TMT_9PLEX_128N(false, false, false), //
+	TMT_9PLEX_128C(false, false, false), //
+	TMT_9PLEX_129N(false, false, false), //
+	TMT_9PLEX_129C(false, false, false), //
+	TMT_9PLEX_130N(false, false, false), //
+	TMT_9PLEX_130C(false, false, true), //
+	//
 	TMT_10PLEX_126(true, false, false), //
 	TMT_10PLEX_127N(false, false, false), //
 	TMT_10PLEX_127C(false, false, false), //
@@ -65,17 +75,25 @@ public enum QuantificationLabel {
 	TMT_16PLEX_134(false, false, true), //
 	//
 	N14(true, false, false), //
-	N15(false, false, true);
+	N15(false, false, true), //
+
+	TMT_CHANNEL(false, false, false);
 
 	private final static Logger log = Logger.getLogger(QuantificationLabel.class);
 	private final boolean isLight;
 	private final boolean isMedium;
 	private final boolean isHeavy;
+	private String customNameForTMT;
 
 	private QuantificationLabel(boolean isLight, boolean isMedium, boolean isHeavy) {
+		this(isLight, isMedium, isHeavy, null);
+	}
+
+	private QuantificationLabel(boolean isLight, boolean isMedium, boolean isHeavy, String customName) {
 		this.isLight = isLight;
 		this.isMedium = isMedium;
 		this.isHeavy = isHeavy;
+		this.setCustomNameForTMT(customName);
 	}
 
 	public static QuantificationLabel getByName(String labelName) {
@@ -148,6 +166,10 @@ public enum QuantificationLabel {
 		return getTMT6PlexLabels().contains(label);
 	}
 
+	public static boolean isTMT9PLEX(QuantificationLabel label) {
+		return getTMT9PlexLabels().contains(label);
+	}
+
 	public static boolean isTMT10PLEX(QuantificationLabel label) {
 		return getTMT10PlexLabels().contains(label);
 	}
@@ -174,6 +196,19 @@ public enum QuantificationLabel {
 		final QuantificationLabel[] array = { QuantificationLabel.TMT_6PLEX_126, QuantificationLabel.TMT_6PLEX_127,
 				QuantificationLabel.TMT_6PLEX_128, QuantificationLabel.TMT_6PLEX_129, QuantificationLabel.TMT_6PLEX_130,
 				QuantificationLabel.TMT_6PLEX_131 };
+		final List<QuantificationLabel> ret = new ArrayList<QuantificationLabel>();
+		for (final QuantificationLabel quantificationLabel : array) {
+			ret.add(quantificationLabel);
+		}
+		return ret;
+	}
+
+	public static List<QuantificationLabel> getTMT9PlexLabels() {
+		final QuantificationLabel[] array = { QuantificationLabel.TMT_9PLEX_126, QuantificationLabel.TMT_9PLEX_127N,
+				QuantificationLabel.TMT_9PLEX_127C, QuantificationLabel.TMT_9PLEX_128N,
+				QuantificationLabel.TMT_9PLEX_128C, QuantificationLabel.TMT_9PLEX_129N,
+				QuantificationLabel.TMT_9PLEX_129C, QuantificationLabel.TMT_9PLEX_130N,
+				QuantificationLabel.TMT_9PLEX_130C };
 		final List<QuantificationLabel> ret = new ArrayList<QuantificationLabel>();
 		for (final QuantificationLabel quantificationLabel : array) {
 			ret.add(quantificationLabel);
@@ -224,11 +259,19 @@ public enum QuantificationLabel {
 		return ret;
 	}
 
+	/**
+	 * 
+	 * @param plex
+	 * @param tmtHeaders the actual headers we read from data file
+	 * @return
+	 */
 	public static List<QuantificationLabel> getTMTPlexLabels(int plex) {
 		if (plex == 4) {
 			return getTMT4PlexLabels();
 		} else if (plex == 6) {
 			return getTMT6PlexLabels();
+		} else if (plex == 9) {
+			return getTMT9PlexLabels();
 		} else if (plex == 10) {
 			return getTMT10PlexLabels();
 		} else if (plex == 11) {
@@ -236,6 +279,7 @@ public enum QuantificationLabel {
 		} else if (plex == 16) {
 			return getTMT16PlexLabels();
 		}
+
 		throw new IllegalArgumentException(
 				"TMT plex " + plex + " is not yet supported. Contact salvador@scripps.edu to fix that!");
 	}
@@ -245,6 +289,8 @@ public enum QuantificationLabel {
 			return 4;
 		} else if (getTMT6PlexLabels().contains(label)) {
 			return 6;
+		} else if (getTMT9PlexLabels().contains(label)) {
+			return 9;
 		} else if (getTMT10PlexLabels().contains(label)) {
 			return 10;
 		} else if (getTMT11PlexLabels().contains(label)) {
@@ -253,5 +299,13 @@ public enum QuantificationLabel {
 			return 16;
 		}
 		throw new IllegalArgumentException("Label " + label + " is not detected as a TMT label");
+	}
+
+	public String getCustomNameForTMT() {
+		return customNameForTMT;
+	}
+
+	public void setCustomNameForTMT(String customNameForTMT) {
+		this.customNameForTMT = customNameForTMT;
 	}
 }
